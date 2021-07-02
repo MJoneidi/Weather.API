a639c138f6ad90b2570e3bca4a76a4dc41eb0b53
Mohammad Joneidi <mjoneidi@accessholding.com>
2021-07-02 08:01:49 +0200

    added a unit test project

---
+++ Tests/Weather.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Weather.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
